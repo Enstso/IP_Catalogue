9bee3b42f030bfbfb27519b73539de079c431fa6
Enstso <enstso@outlook.fr>
2023-11-14 22:56:41 +0100

    LAST COMMIT

--- main.c
+++ main.c
@@ -102,7 +102,6 @@ void hexa(unsigned int ipTab[4])
         while (number)
         {
             unsigned int calc = number % 16;
-
             if (calc < 10)
             {
                 hexa[index] = calc + '0';
@@ -153,7 +152,7 @@ void bibine(unsigned int n,unsigned int tab[33], unsigned int size)
 void networkAdress(unsigned int ip[33], unsigned int mask[33], unsigned int networkAdress[33])
 {
     unsigned int networkSum = 0;
-    for (int i = 0; i < 33; i++)
+    for (int i = 0; i < 32; i++)
     {
         if (ip[i] + mask[i] == 2)
         {
@@ -169,7 +168,7 @@ void networkAdress(unsigned int ip[33], unsigned int mask[33], unsigned int netw
 unsigned int sameNetwork(unsigned int tab1[33], unsigned int tab2[33])
 {
     unsigned int verif = 0;
-    for (int i = 0; i < 33; i++)
+    for (int i = 0; i < 32; i++)
     {
         if (tab1[i] != tab2[i])
         {
